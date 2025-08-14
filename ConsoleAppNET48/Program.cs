22ed4b4565f13219eda695318a1464789f7c0954
Freddy Juhel <ext.freddy.juhel@reseau.sncf.fr>
2025-08-14 10:47:32 +0200

    add a console application project

---
+++ ConsoleAppNET48/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleAppNET48
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      // This is a .NET Framework 4.8 console application.
+      // It can be used to test compatibility with .NET Framework 4.8.
+      Console.WriteLine("Hello from .NET Framework 4.8 console application");
+      Console.WriteLine("This application can only be run once as it applies the singleton pattern");
+
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
